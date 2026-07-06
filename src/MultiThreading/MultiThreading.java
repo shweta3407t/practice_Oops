c740fac7df6dea0dcc4c4c72ac63dff13a242ab9
Shweta Prajapati <shweta3407t@gmail.com>
2026-07-06 20:04:20 +0530

    basics of Multithreading

---
+++ src/MultiThreading/MultiThreading.java
@@ -0,0 +1,64 @@
+package MultiThreading;
+
+public class MultiThreading {
+    static void main() {
+//        MyThread t1=new MyThread();
+//        t1.start( );
+//
+//
+//        MyRunnable r=new MyRunnable();
+//        Thread t2=new Thread(r);
+//        t2.start();
+//
+//        //lambda
+//        Thread t3=new Thread(() -> System.out.println("lambda thread is running"));
+//        t3.start();
+
+//        Thread t4=new Thread( () -> {
+//            System.out.println(Thread.currentThread().getName());
+//        }) ;
+//        t4.start();
+//        t4.run();//it will show main thread name
+
+        Thread t5=new Thread(() -> {
+            for(int i=1 ; i< 10 ; i++){
+                if(i %2 == 0) {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {}
+                    System.out.println( "t1 ="+i);
+                }
+            }
+        });
+        t5.start();
+
+        Thread t6=new Thread(() -> {
+            for(int i=1 ; i< 10 ; i++){
+                if(i %2 != 0) {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {}
+                    System.out.println( "t2 ="+i);
+                }
+            }
+        });
+        t6.start();
+
+
+     }
+}
+
+class MyThread extends Thread{
+    public   void run(){
+        System.out.println("Thread is running");
+    }
+}
+
+class MyRunnable implements  Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("Runnable thread is running");
+
+    }
+}
